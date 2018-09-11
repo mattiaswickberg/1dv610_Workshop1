23cb25c67c31abf6e5a6cf6b63bdff158e9a9f60
Mattias Wickberg <ad13113@ad.ssis.nu>
2018-09-11 11:09:18 +0200

    added stylesheet

--- ws1.php
+++ ws1.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <?php 
@@ -10,7 +11,7 @@ $text="";
 <form method="post" action="reverse.php">
 
 Text: <br>
-<textarea name="text" rows="10" cols="40"></textarea>
+<textarea name="text" rows="10" cols="40"></textarea><br>
 <input type="submit">
 </form>
 </body>
